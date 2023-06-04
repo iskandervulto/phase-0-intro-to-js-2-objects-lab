57b2083887561f6f8f69fa2239d2301aaecbe64e
iskandervulto <iskanderhyvulto@gmail.com>
2023-06-04 15:09:51 -0400

    C

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Write your solution in this file!
+const employee = {
+    name: `Bob`,
+    streetAddress: "123",
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+  }
+
+  beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+  }
+
+  beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function deleteFromEmployeeByKey(employee, key){
+    const clonedEmployee = {...employee};
+    delete clonedEmployee[key];
+    return clonedEmployee
+  }
+
+  beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee
+  }
+
